40617c9f4c504eb70acc681357c4167eb6b90b03
kentaro-m <kentaro.m811@gmail.com>
2022-02-05 23:13:21 +0900

    feat: add function to return random permutation of array of string

---
+++ shuffle.ts
@@ -0,0 +1,10 @@
+type Shuffle = (arr: string[]) => string[];
+
+export const shuffle: Shuffle = ([...arr]) => {
+  let m = arr.length
+  while (m) {
+    const i = Math.floor(Math.random() * m--)
+    ;[arr[m], arr[i]] = [arr[i], arr[m]]
+  }
+  return arr
+}
